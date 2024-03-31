8012010aa8163613e4d8505b63486ff9ab187130
Alex <alexsoutes@yandex.ru>
2024-03-31 18:50:31 +0300

    Добавление иконок, экран информация о вакансии. #2

--- app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/VacancyFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/VacancyFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class VacancyFragment : Fragment() {
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return super.onCreateView(inflater, container, savedInstanceState)
     }
