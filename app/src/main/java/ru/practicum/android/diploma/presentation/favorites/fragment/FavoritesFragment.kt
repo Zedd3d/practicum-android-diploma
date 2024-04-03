5791c9c8719259b87742914d724b5da69fdf8fe2
PanBuz <aleksandor1203@yandex.ru>
2024-04-03 16:30:23 +0500

    Переопределил метод onResume в фрагментах избранного и команды

--- app/src/main/java/ru/practicum/android/diploma/presentation/favorites/fragment/FavoritesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/favorites/fragment/FavoritesFragment.kt
@@ -2,9 +2,11 @@ package ru.practicum.android.diploma.presentation.favorites.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.app.App
 import ru.practicum.android.diploma.databinding.FragmentFavoritesBinding
@@ -23,5 +25,9 @@ class FavoritesFragment : Fragment(R.layout.fragment_favorites) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+    }
+    override fun onResume() {
+        super.onResume()
+        activity?.findViewById<BottomNavigationView>(R.id.bottom_navigation)?.isVisible = true
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/presentation/team/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/team/TeamFragment.kt
@@ -1,8 +1,14 @@
 package ru.practicum.android.diploma.presentation.team
 
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.practicum.android.diploma.R
 
 class TeamFragment : Fragment(R.layout.fragment_team) {
 
+    override fun onResume() {
+        super.onResume()
+        activity?.findViewById<BottomNavigationView>(R.id.bottom_navigation)?.isVisible = true
+    }
 }
