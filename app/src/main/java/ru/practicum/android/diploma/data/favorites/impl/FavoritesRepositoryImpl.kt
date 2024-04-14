097e859e399a7cd58257b6da90880e790ae744f6
Zedd3d <yariyan@yandex.ru>
2024-04-14 12:13:27 +0300

    Устранение недочетов по ревью

--- app/src/main/java/ru/practicum/android/diploma/data/favorites/impl/FavoritesRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/favorites/impl/FavoritesRepositoryImpl.kt
@@ -1,11 +1,13 @@
 package ru.practicum.android.diploma.data.favorites.impl
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import ru.practicum.android.diploma.data.converters.VacancyDbConvertor
 import ru.practicum.android.diploma.data.db.AppDatabase
 import ru.practicum.android.diploma.data.favorites.entity.FavoritesVacanciesEntity
 import ru.practicum.android.diploma.domain.favorites.api.FavoritesRepository
+import ru.practicum.android.diploma.domain.models.Salary
 import ru.practicum.android.diploma.domain.models.Vacancy
 import ru.practicum.android.diploma.domain.models.VacancyDetail
 import javax.inject.Inject
@@ -35,15 +37,55 @@ class FavoritesRepositoryImpl @Inject constructor(
         return if (list.isEmpty()) {
             null
         } else {
-            VacancyDbConvertor.mapToVacancyDetail(list.first())
+            list.first().mapToVacancyDetail()
         }
     }
 
     private fun convertFromVacancyEntity(vacancyList: List<FavoritesVacanciesEntity>): List<Vacancy> {
-        return vacancyList.map { vac -> VacancyDbConvertor.mapToVacancy(vac) }
+        return vacancyList.map { vac -> vac.mapToDomain() }
     }
 
     private fun convertToVacancyEntity(vacancyDetail: VacancyDetail): FavoritesVacanciesEntity {
-        return VacancyDbConvertor.map(vacancyDetail)
+        return vacancyDetail.mapToDomain()
     }
+}
+
+private fun FavoritesVacanciesEntity.mapToVacancyDetail(): VacancyDetail {
+    val gson = Gson()
+    return gson.fromJson<VacancyDetail>(
+        vacancyDetailData,
+        object : TypeToken<VacancyDetail>() {}.type
+    )
+}
+
+private fun VacancyDetail.mapToDomain(): FavoritesVacanciesEntity {
+    val gson = Gson()
+    return FavoritesVacanciesEntity(
+        id,
+        employer?.logoUrls ?: "",
+        employer?.name ?: "",
+        name,
+        area ?: "",
+        gson.toJson(this),
+        salary?.currency ?: "",
+        salary?.from ?: 0,
+        salary?.to ?: 0,
+        salary?.gross ?: false
+    )
+}
+
+private fun FavoritesVacanciesEntity.mapToDomain(): Vacancy {
+    return Vacancy(
+        id,
+        img,
+        employer,
+        name,
+        Salary(
+            salaryCurrency,
+            salaryFrom,
+            salaryGross,
+            salaryTo
+        ),
+        area
+    )
 }
--- app/src/main/java/ru/practicum/android/diploma/domain/sharedpreferences/api/SharedPreferencesRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/sharedpreferences/api/SharedPreferencesRepository.kt
@@ -3,7 +3,7 @@ package ru.practicum.android.diploma.domain.sharedpreferences.api
 import ru.practicum.android.diploma.domain.filters.models.FilterValue
 
 interface SharedPreferencesRepository {
-    fun getAllFilters(): HashMap<String, String>
+    fun getAllFilters(): Map<String, String>
 
     fun getFilter(filterName: String): FilterValue?
 
